42db28b507b1a13ee14051fd6fa797727cc97a02
Eric Oliveira <ericjmoliveira@gmail.com>
2023-08-05 11:26:43 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,103 +4,22 @@ import { ClubUseCases } from './use-cases/clubs';
 import { PrismaRepository } from './repositories/prisma';
 
 import { prisma } from './lib/prisma';
+import { ClubController } from './controllers/club';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const prismaRepository = new PrismaRepository(prisma);
+const clubUseCases = new ClubUseCases(prismaRepository);
+const clubController = new ClubController(clubUseCases);
+
 app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Hello world!' }));
-
-app.get('/clubs', async (req, res) => {
-  try {
-    const prismaRepository = new PrismaRepository(prisma);
-    const clubUseCases = new ClubUseCases(prismaRepository);
-
-    const clubs = await clubUseCases.getAllClubs();
-
-    if (clubs) {
-      return res.status(200).json({ data: { clubs } });
-    }
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.get('/clubs/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const prismaRepository = new PrismaRepository(prisma);
-    const clubUseCases = new ClubUseCases(prismaRepository);
-
-    const club = await clubUseCases.getClub(id);
-
-    if (!club) {
-      return res.status(404).json({ error: 'Club not found' });
-    }
-
-    return res.status(200).json({ data: { club } });
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.post('/clubs', async (req, res) => {
-  try {
-    const { name, stadium, manager } = req.body;
-
-    if (!name) return res.status(400).json({ error: 'Missing club name' });
-    if (!stadium) return res.status(400).json({ error: 'Missing club stadium' });
-    if (!manager) return res.status(400).json({ error: 'Missing club manager' });
-
-    const prismaRepository = new PrismaRepository(prisma);
-    const clubUseCases = new ClubUseCases(prismaRepository);
-
-    const club = await clubUseCases.createClub({ name, stadium, manager });
-
-    return res.status(201).json({ data: { club }, message: 'Club created' });
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.put('/clubs/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const { name, stadium, manager } = req.body;
-
-    if (!name && !stadium && !manager) {
-      return res.status(400).json({ error: 'Missing update data' });
-    }
-
-    const prismaRepository = new PrismaRepository(prisma);
-    const clubUseCases = new ClubUseCases(prismaRepository);
-
-    const club = await clubUseCases.updateClub(id, { name, stadium, manager });
-
-    return res.status(200).json({ data: { club }, message: 'Club updated' });
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.delete('/clubs/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const prismaRepository = new PrismaRepository(prisma);
-    const clubUseCases = new ClubUseCases(prismaRepository);
-
-    const deletedClub = await clubUseCases.deleteClub(id);
-
-    if (deletedClub) {
-      return res.status(200).json({ message: 'Club deleted' });
-    }
-  } catch (error) {
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
+app.get('/clubs', (req, res) => clubController.getClubs(req, res));
+app.get('/clubs/:id', async (req, res) => clubController.getClub(req, res));
+app.post('/clubs', async (req, res) => clubController.createClub(req, res));
+app.put('/clubs/:id', async (req, res) => clubController.updateClub(req, res));
+app.delete('/clubs/:id', (req, res) => clubController.deleteClub(req, res));
 
 app.listen(port, () => console.log(`App listening on port ${5000}`));
