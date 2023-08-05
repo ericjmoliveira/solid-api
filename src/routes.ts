1943363059cd17665edc7e79e51ccdba07647a5f
Eric Oliveira <96954599+ericjmoliveira@users.noreply.github.com>
2023-08-05 12:11:38 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -12,4 +12,4 @@ routes.get('/clubs', (req, res) => clubController.getClubs(req, res));
 routes.get('/clubs/:id', async (req, res) => clubController.getClub(req, res));
 routes.post('/clubs', async (req, res) => clubController.createClub(req, res));
 routes.put('/clubs/:id', async (req, res) => clubController.updateClub(req, res));
-routes.delete('/clubs/:id', (req, res) => clubController.deleteClub(req, res));
+routes.delete('/clubs/:id', async (req, res) => clubController.deleteClub(req, res));
