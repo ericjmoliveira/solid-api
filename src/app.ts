f5964f26f436d399eaca537dd2e731fa174fbf92
Eric Oliveira <96954599+ericjmoliveira@users.noreply.github.com>
2023-08-05 12:11:14 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,4 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`App listening on port ${5000}`));
+app.listen(port, () => console.log(`App listening on port ${port}`));
