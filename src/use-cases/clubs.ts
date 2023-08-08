df11728a46f1c04da8f13af87ad3ff6bdce5d620
Eric Oliveira <ericjmoliveira@gmail.com>
2023-08-08 11:10:44 -0300

    Update club use cases

--- src/use-cases/clubs.ts
+++ src/use-cases/clubs.ts
@@ -4,23 +4,23 @@ import { ClubRepository } from '../interfaces/club-repository';
 export class ClubUseCases {
   constructor(private clubRepository: ClubRepository) {}
 
-  async getAllClubs() {
+  getAllClubs() {
     return this.clubRepository.getAllClubs();
   }
 
-  async getClub(id: string) {
+  getClub(id: string) {
     return this.clubRepository.getClub(id);
   }
 
-  async createClub(data: Club) {
+  createClub(data: Club) {
     return this.clubRepository.createClub(data);
   }
 
-  async updateClub(id: string, data: Club) {
+  updateClub(id: string, data: Club) {
     return this.clubRepository.updateClub(id, data);
   }
 
-  async deleteClub(id: string) {
+  deleteClub(id: string) {
     return this.clubRepository.deleteClub(id);
   }
 }
